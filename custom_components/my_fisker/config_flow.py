f7fc5b03647be848aa28771643588dc4e3122a80
MichaelOE <37800126+MichaelOE@users.noreply.github.com>
2024-01-16 17:11:30 +0100

    Update config_flow.py

--- custom_components/my_fisker/config_flow.py
+++ custom_components/my_fisker/config_flow.py
@@ -7,7 +7,7 @@ from typing import Any
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_ALIAS, CONF_HOST, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
@@ -18,16 +18,20 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-# TODO adjust the data schema to the data that you need
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_USERNAME): str,
         vol.Required(CONF_PASSWORD): str,
     }
 )
+STEP_ALIAS_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_ALIAS): str,
+    }
+)
 
 
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+async def validate_login(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
     """Validate the user input allows us to connect.
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
@@ -47,7 +51,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
     # InvalidAuth
 
     # Return info that you want to store in the config entry.
-    return {"title": f"My Fisker Ocean ({vin})"}
+    return vin  # {"title": f"My Fisker Ocean ({vin})"}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -62,7 +66,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
         if user_input is not None:
             try:
-                info = await validate_input(self.hass, user_input)
+                self._userInput = user_input
+                self._userInput["vin"] = await validate_login(self.hass, self._userInput)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except InvalidAuth:
@@ -71,10 +76,37 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
             else:
-                return self.async_create_entry(title=info["title"], data=user_input)
+                return await self.async_step_alias()
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_alias(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the alias step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            try:
+                # info = await validate_input(self.hass, user_input)
+                self._userInput[CONF_ALIAS] = user_input[CONF_ALIAS]
+                _LOGGER.info(user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(
+                    title=f"{self._userInput[CONF_ALIAS]}  ({self._userInput['vin']})",
+                    data=self._userInput,
+                )
+
+        return self.async_show_form(
+            step_id="alias", data_schema=STEP_ALIAS_DATA_SCHEMA, errors=errors
         )
 
 
