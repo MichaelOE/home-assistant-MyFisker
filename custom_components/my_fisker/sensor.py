83f229a43fc708ad3762d0ae5570375ed0d99970
MichaelOE <37800126+MichaelOE@users.noreply.github.com>
2024-01-31 08:03:02 +0100

    Update sensor.py

--- custom_components/my_fisker/sensor.py
+++ custom_components/my_fisker/sensor.py
@@ -8,7 +8,6 @@ import json
 import logging
 from typing import cast
 
-from my_fisker import FiskerEntityDescription
 from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorEntity,
