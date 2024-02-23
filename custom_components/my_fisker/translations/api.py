60de98508c4820a002db2a7294454f00e84f2ffb
MichaelOE <37800126+MichaelOE@users.noreply.github.com>
2024-02-23 21:34:47 +0100

    Added 'region' to config_flow
    Added 'region' to config_flow

---
+++ custom_components/my_fisker/translations/api.py
@@ -0,0 +1,289 @@
+# import asyncio
+import json
+import logging
+
+import aiohttp
+
+# import requests
+from .const import API_TIMEOUT, TOKEN_URL, WSS_URL_EU, WSS_URL_US
+
+# import websockets
+
+_LOGGER = logging.getLogger(__name__)
+
+headers = {"User-Agent": "MOBILE 1.0.0.0"}
+
+HasAUTH = False
+HasVIN = False
+vin = ""
+
+
+class MyFiskerAPI:
+    """Handle connection towards Fisker API servers."""
+
+    def __init__(self, username: str, password: str, region: str):
+        _LOGGER.debug("MyFiskerAPI init")
+        self._username = username
+        self._password = password
+        self._region = region
+
+        self._token = ""
+        self._timeout = aiohttp.ClientTimeout(total=API_TIMEOUT)
+        self.data = {}
+
+    async def GetAuthTokenAsync(self):
+        """Get the Authentification token from Fisker, is used towards the WebSocket connection."""
+
+        params = {"username": self._username, "password": self._password}
+        async with aiohttp.ClientSession() as session, session.post(
+            TOKEN_URL, data=params
+        ) as response:
+            data = await response.json()
+
+            # Check if a key exists
+            if "accessToken" in data:
+                retVal = data["accessToken"]
+            else:
+                retVal = data["message"]
+
+            self._token = retVal
+            return self._token
+
+    async def tokenReturn(self):
+        return self._token
+
+    def GetCarSettings(self):
+        try:
+            data = json.loads(self.data["car_settings"])
+            return self.flatten_json(data["data"])
+        except NameError:
+            _LOGGER.warning("Self.data['car_settings'] is not available")
+            return None
+
+    async def GetDigitalTwin(self):
+        self.data["digital_twin"] = self.flatten_json(
+            self.ParseDigitalTwinResponse(
+                await self.__GetWebsocketResponse("digital_twin")
+            )
+        )
+        return self.data["digital_twin"]
+
+    async def GetProfiles(self):
+        self.data["profiles"] = self.ParseProfilesResponse(
+            await self.__GetWebsocketResponse("profiles")
+        )
+        return self.data["profiles"]
+
+    def ParseDigitalTwinResponse(self, jsonMsg):
+        # _LOGGER.debug('Start ParseDigitalTwinResponse()')
+        # Parse the JSON response into a Python dictionary
+        data = json.loads(jsonMsg)
+        # print (data)
+        if data["handler"] != "digital_twin":
+            _LOGGER.debug("ParseDigitalTwinResponse: Wrong answer from websocket")
+            _LOGGER.debug(data)
+            return "Wrong answer from websocket"
+
+        # Now you can access the items in the JSON response as you would with a Python dictionary
+        digital_twin = data["data"]
+
+        # Use the jsonpath expression to find the value in the data
+        _LOGGER.debug(digital_twin)  # Outputs: value1
+        return digital_twin
+
+    def GenerateVerifyRequest(self):
+        # _LOGGER.debug('Start GenerateVerifyRequest()')
+        data = {}
+        messageData = {}
+
+        # token = self.GetAuthToken(username, password)
+        token = self._token
+
+        data["token"] = token
+        messageData["data"] = data
+        messageData["handler"] = "verify"
+        # print (messageData)
+        return messageData
+
+    def ParseVerifyResponse(self, jsonMsg):
+        # Parse the JSON response into a Python dictionary
+        data = json.loads(jsonMsg)
+
+        if data["handler"] != "verify":
+            return "Wrong answer from websocket"
+
+        # Now you can access the items in the JSON response as you would with a Python dictionary
+        item1 = data["data"]["authenticated"]
+
+        if item1 != "true":
+            return "Not authenticated"
+
+        result = item1
+        _LOGGER.debug(result)  # Outputs: value1
+        return True
+
+    def GenerateProfilesRequest(self):
+        # _LOGGER.debug('Start GenerateProfilesRequest()')
+        messageData = {}
+
+        messageData["handler"] = "profiles"
+        # print (messageData)
+        return messageData
+
+    def DigitalTwinRequest(self, vin):
+        # _LOGGER.debug('Start DigitalTwinRequest()')
+        data = {}
+        messageData = {}
+        data["vin"] = vin
+        messageData["data"] = data
+        messageData["handler"] = "digital_twin"
+        return messageData
+
+    def ParseProfilesResponse(self, jsonMsg):
+        # _LOGGER.debug('Start ParseProfilesResponse()')
+        # Parse the JSON response into a Python dictionary
+        data = json.loads(jsonMsg)
+        # print (data)
+        if data["handler"] != "profiles":
+            _LOGGER.debug("ParseProfilesResponse: Wrong answer from websocket")
+            _LOGGER.debug(data)
+            return "Wrong answer from websocket"
+
+        # Now you can access the items in the JSON response as you would with a Python dictionary
+        item1 = data["data"][0]["vin"]
+
+        # Use the jsonpath expression to find the value in the data
+        result = item1
+        return result
+
+    def SendCommandRequest(self, command):
+        # _LOGGER.debug('Start SendCommandRequest()')
+        data = {}
+        messageData = {}
+        data["vin"] = vin
+        messageData["data"] = data
+        messageData["handler"] = "remote_command"
+        return messageData
+
+    def __GetRegionURL(self):
+        match self._region:
+            case "EU":
+                return WSS_URL_EU
+            case "US":
+                return WSS_URL_US
+            case _:
+                return WSS_URL_US
+
+    async def __GetWebsocketResponse(self, responseToReturn: str):
+        HasAUTH = False
+        HasVIN = HasAUTH
+
+        wssUrl = self.__GetRegionURL()
+
+        async with aiohttp.ClientSession() as session:
+            async with session.ws_connect(wssUrl, headers=headers) as ws:
+                await ws.send_str(json.dumps(self.GenerateVerifyRequest()))
+                while True:
+                    response = await ws.receive_str()
+                    handler = json.loads(response)["handler"]
+
+                    if handler == "car_settings":
+                        self.data["car_settings"] = response
+
+                    if handler == responseToReturn:
+                        try:
+                            await ws.close()
+                        except Exception as e:
+                            _LOGGER.debug(
+                                f"Error occurred while closing WebSocket: {e}"
+                            )
+                        return response
+
+                    if HasAUTH is not True:
+                        if handler == "verify":
+                            HasAUTH = (
+                                json.loads(response)["data"]["authenticated"] is True
+                            )
+                            # Send a message
+                            # _LOGGER.debug(f"Sending 'GenerateProfilesRequest'")
+                            await ws.send_str(
+                                json.dumps(self.GenerateProfilesRequest())
+                            )
+
+                    if HasAUTH is True and HasVIN is not True:
+                        if handler == "profiles":
+                            vin = self.ParseProfilesResponse(response)
+                            # print (f"vin = {vin}")
+                            if vin != "":
+                                self.HasVIN = True
+                                # Send a message
+                                _LOGGER.debug(
+                                    f"Auth & VIN ok - Sending 'DigitalTwinRequest' to vin={vin}"
+                                )
+                                await ws.send_str(
+                                    json.dumps(self.DigitalTwinRequest(vin))
+                                )
+
+                    if HasAUTH is True and HasVIN is True:
+                        # _LOGGER.debug(f"Received message: {message}")
+                        if handler == responseToReturn:
+                            # self.ParseDigitalTwinResponse(response)
+                            _LOGGER.error(response)
+                            try:
+                                await ws.close()
+                            except Exception as e:
+                                _LOGGER.error(
+                                    f"Error occurred while closing WebSocket: {e}"
+                                )
+
+                            return response
+
+    def flatten_json(self, jsonIn):
+        out = {}
+
+        def flatten(x, name=""):
+            if type(x) is dict:
+                for a in x:
+                    flatten(x[a], name + a + "_")
+            elif type(x) is list:
+                i = 0
+                for a in x:
+                    flatten(a, name + str(i) + "_")
+                    i += 1
+            else:
+                out[name[:-1]] = x
+
+        flatten(jsonIn)
+        return out
+
+
+class MyFiskerApiError(Exception):
+    """Base exception for all MyFisker API errors"""
+
+
+class AuthenticationError(MyFiskerApiError):
+    """Authenatication failed"""
+
+
+class RequestError(MyFiskerApiError):
+    """Failed to get the results from the API"""
+
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+
+class RequestConnectionError(MyFiskerApiError):
+    """Failed to make the request to the API"""
+
+
+class RequestTimeoutError(MyFiskerApiError):
+    """Failed to get the results from the API"""
+
+
+class RequestRetryError(MyFiskerApiError):
+    """Retries too many times"""
+
+
+class RequestDataError(MyFiskerApiError):
+    """Data is not valid"""
---
+++ custom_components/my_fisker/translations/config_flow.py
@@ -0,0 +1,114 @@
+"""Config flow for My Fisker integration."""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_ALIAS, CONF_PASSWORD, CONF_REGION, CONF_USERNAME
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.exceptions import HomeAssistantError
+
+# from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from .api import MyFiskerAPI
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_USERNAME): str,
+        vol.Required(CONF_PASSWORD): str,
+        vol.Required(CONF_REGION): vol.In(["EU", "US", "Other"]),
+    }
+)
+STEP_ALIAS_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_ALIAS): str,
+    }
+)
+
+
+async def validate_login(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the user input allows us to connect. Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user."""
+
+    api = MyFiskerAPI(data[CONF_USERNAME], data[CONF_PASSWORD], data[CONF_REGION])
+
+    try:
+        res = await api.GetAuthTokenAsync()
+        if len(res) < 50:
+            raise InvalidAuth
+    except:
+        raise InvalidAuth
+
+    try:
+        vin = await api.GetProfiles()
+    except:
+        raise CannotConnect
+
+    # Return info that you want to store in the config entry.
+    return vin
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for My Fisker."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            try:
+                self._userInput = user_input
+                self._userInput["vin"] = await validate_login(
+                    self.hass, self._userInput
+                )
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return await self.async_step_alias()
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_alias(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the alias step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            try:
+                self._userInput[CONF_ALIAS] = user_input[CONF_ALIAS]
+                _LOGGER.info(user_input)
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(
+                    title=f"{self._userInput[CONF_ALIAS]}  ({self._userInput['vin']})",
+                    data=self._userInput,
+                )
+
+        return self.async_show_form(
+            step_id="alias", data_schema=STEP_ALIAS_DATA_SCHEMA, errors=errors
+        )
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(HomeAssistantError):
+    """Error to indicate there is invalid auth."""
